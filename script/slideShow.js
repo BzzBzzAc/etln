eda37e7a9dea8befa6f9095389f25a3edda098cd
BzzBzzAc <105174632+BzzBzzAc@users.noreply.github.com>
2022-05-09 10:51:32 +0200

    Add files via upload

---
+++ script/slideShow.js
@@ -0,0 +1,30 @@
+let slideIndex = 0;
+showSlides();
+
+
+function currentSlide(num){
+    showSlides(slideIndex = num)
+}
+
+function showSlides(num){
+    let i;
+    let slide = document.getElementsByClassName("mySlide");
+    let dots = document.getElementsByClassName("dot");
+    // if(num < 1){
+    //     slideIndex = slide.length
+    // }
+    for(i = 0; i < slide.length; i++){
+        slide[i].style.display = "none"
+    }
+    slideIndex++;
+    if(slideIndex > slide.length){
+        slideIndex = 1
+    }
+    for(i = 0; i < dots.length;i++){
+        dots[i].className = dots[i].className.replace(" active-dot", "")
+    }
+
+        slide[slideIndex - 1].style.display = "block"
+        dots[slideIndex - 1].className += " active-dot"
+        setTimeout(showSlides, 5000);
+}
